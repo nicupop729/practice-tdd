327c899d743f5b7da332dda4adb6263d2f64146e
Nicolae Pop <nicupop729@gmail.com>
2022-02-03 17:38:08 -0300

    Test the reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,7 +5,7 @@ describe 'test a new instance of Solver' do
     @new_solver = Solver.new
   end
 
-  contex 'test the factorial '
+  context 'test the factorial method'
   it 'returns the rigth answer for 5' do
     expect(@new_solver.factorial(5)).to eq(120)
   end
@@ -16,5 +16,22 @@ describe 'test a new instance of Solver' do
 
   it 'returns an error message for negative number' do
     expect(@new_solver.factorial(-2)).to eq(nil)
+  end
+
+  context 'test the reverse method'
+  it 'returns the right result for "hello"' do
+    expect(@new_solver.reverse('hello')).to eq('olleh')
+  end
+
+  it 'returns the right result for "abracadabra"' do
+    expect(@new_solver.reverse('abracadabra')).to eq('arbadacarba')
+  end
+
+  it 'returns the right result for "12345"' do
+    expect(@new_solver.reverse('12345')).to eq('54321')
+  end
+
+  it 'returns the right result for 54321' do
+    expect { @new_solver.reverse(54321) } .to raise_error(NoMethodError)
   end
 end
